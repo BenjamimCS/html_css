d7398ff1d9dfc9ef95c16cf47d4e7a510be3b423
BenjamimCS <83431838+BenjamimCS@users.noreply.github.com>
2022-01-24 02:24:47 -0300

    Fazendo Um Formulário

---
+++ praticas/formulario/valid.php
@@ -0,0 +1,39 @@
+<?php
+
+    $email = $_POST["id-email"];
+    $pass = $_POST["id-pass"];
+
+    echo 
+    "
+    Email: $email<br>
+    Senha: <span>$pass";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dados Inseridos</title>
+</head>
+<body>
+    <header>
+		<h1>Logo</h1>
+		<nav>
+			<ul>
+				<li>
+					<a href="#">Home</a>
+				<li>
+					<a href="#">Novidades</a>
+				<li>
+					<a href="#">FAQ</a>
+				<li>
+					<a href="#">Sobre</a>
+			</ul>
+		</nav>
+	</header>
+    <main>
+        
+    </main>
+</body>
+</html>
